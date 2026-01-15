af2505d6817d539b21651bb558667f55a58e15e8
Martins <aremartins801@gmail.com>
2026-01-15 20:27:45 +0000

    fix

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 const whatsapp = require('./whatsapp');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
